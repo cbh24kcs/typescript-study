ff10936a28ce5f5cdb9569ce3d6602deab0c6b26
cbh24kcs <479466796@qq.com>
2022-06-27 22:05:06 +0800

    update

---
+++ ts-001/dist/a.js
@@ -0,0 +1,14 @@
+"use strict";
+var a = "Hello";
+function fun(s) {
+}
+var fun2 = function (x) { return x * x; };
+fun(a);
+fun(3);
+// interface Student extends Person, A {}
+var arr = [
+    { name: '张三', age: 18, x: 3 },
+];
+// 类型推断
+var x = 3;
+// x = '4'
---
+++ ts-001/src/a.ts
@@ -0,0 +1,33 @@
+let a: string = "Hello";
+
+function fun(s: number | string) {
+}
+
+let fun2 = (x: number) => x * x;
+
+
+fun(a);
+fun(3);
+
+interface Person {
+    name: string,
+    age: number
+}
+
+interface Person {
+    x: number,
+    name: string
+}
+
+
+// interface Student extends Person, A {}
+
+
+let arr: Array<Person> = [
+    {name: '张三', age: 18, x: 3},
+]
+
+// 类型推断
+let x = 3;
+
+// x = '4'
